6b5b0a7b931b35a2692d0af3242a078aff1bb791
JackHarperIMF <hemvayyala8@gmail.com>
2024-03-19 13:59:50 +0530

    data

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const PORT = 8081;
 
 app.get("/", (req,res) => {
     res.status(200).json({
-        message: "The server is up and running in port: ", PORT
+        message: "The server is up and running", PORT
     })
+})
+
+app.get("*", (req,res) => {
+    res.status(404).json({
+        message: "The route does not exist",
+    })
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`The server is up and running on the PORT ${PORT}`);
 })
